0388dd939221855fb611172f28169a56409fd186
HirokiNakano <baseballinobu@gmail.com>
2024-11-23 08:59:53 +0900

    エラー「Tightenco\Ziggy\Ziggy" not found」

---
+++ resources/js/Pages/Inertia/InertiaTest.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+  Inertiaテストです。<br>
+  <a href="/">a tag</a><br>
+  <Link href="/">Link</Link><br>
+  <Link :href="route('inertia.index')">名前付きルート</Link>
+</template>
